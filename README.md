1cbbc9fc07d2b81eca4ca4d68724c232102730fe
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-09-03 13:54:07 +0200

    Adding Custom Auto configuration

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# spring-boot-efficient-development-auto-configuration
+## About
+Spring Boot Just-Gif-It Auto Configuration Module
+
+## Provides
+
+### Services
+* `VideoDecoderService` - `@ConditionalOnMissingBean`
+* `GifEncoderService` - `@ConditionalOnMissingBean`
+* `ConverterService` - `@ConditionalOnMissingBean`
+
+### Properties
+* `com.justgifit.create-result-dir #true/false`
+* `com.justgifit.optimize #true/false`
+
+## License
+Version 2.0 of the Apache License
---
+++ src/main/com.justgifit/JustGifItAutoConfiguration.java
@@ -0,0 +1,96 @@
+package com.justgifit;
+
+import com.justgifit.services.ConverterService;
+import com.justgifit.services.GifEncoderService;
+import com.justgifit.services.VideoDecoderService;
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.filter.RequestContextFilter;
+
+import java.io.File;
+
+@Configuration
+@ConditionalOnClass({FFmpegFrameGrabber.class, AnimatedGifEncoder.class})
+public class JustGifItAutoConfiguration {
+
+    @Value("${multipart.location}/gif/")
+    private String gifLocation;
+
+    @Bean
+    @ConditionalOnProperty(prefix = "com.justgifit", name = "create-result-dir")
+    public boolean createResultDirectory() {
+        File gifFolder = new File(gifLocation);
+        if (!gifFolder.exists()) {
+            gifFolder.mkdir();
+        }
+
+        return true;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(VideoDecoderService.class)
+    public VideoDecoderService videoDecoderService() {
+        return new VideoDecoderService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(GifEncoderService.class)
+    public GifEncoderService gifEncoderService() {
+        return new GifEncoderService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ConverterService.class)
+    public ConverterService converterService() {
+        return new ConverterService();
+    }
+
+    @Configuration
+    @ConditionalOnWebApplication
+    public static class WebConfiguration {
+
+        @Value("${multipart.location}/gif/")
+        private String gifLocation;
+
+
+        /**
+         * Disable default HiddenHttpMethodFilter
+         *
+         * @param filter
+         * @return
+         */
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justgifit", name = "optimize")
+        public FilterRegistrationBean deRegisterHiddenHttpMethodFilter(HiddenHttpMethodFilter filter) {
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justgifit", name = "optimize")
+        public FilterRegistrationBean deRegisterHiddenHttpPutFormContentFilter(HttpPutFormContentFilter filter) {
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justgifit", name = "optimize")
+        public FilterRegistrationBean deRegisterContextFilter(RequestContextFilter filter) {
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+    }
+}
